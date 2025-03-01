83f2c4dee83b607a1b8b0e7db2d38f0b6137a3d6
Sumit Das <sumit.kadigital@gmail.com>
2025-03-01 11:56:37 +0530

    Partially completed the section C

--- client/src/pages/Home.page.tsx
+++ client/src/pages/Home.page.tsx
@@ -1,10 +1,12 @@
 import SectionB from "./SectionB";
+import SectionC from "./SectionC";
 import FormPage from "./Test.page";
 const Homepage = () => {
   return (
     <div className="w-[95vw] m-auto ">
-      <FormPage />
+      {/* <FormPage /> */}
       {/* <SectionB/> */}
+      <SectionC/>
     </div>
   );
 };
