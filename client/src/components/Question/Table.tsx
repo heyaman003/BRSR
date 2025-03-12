c02fcfbfe6292b0fe6e1d29df89307e69aa909a6
heyaman003 <heyaman329@gmail.com>
2025-03-12 17:29:39 +0530

    chnage done for norms

--- client/src/components/Question/Table.tsx
+++ client/src/components/Question/Table.tsx
@@ -60,6 +60,13 @@ const TableUI = ({
       return { ...table };
     });
   };
+  const deleteRow = () => {
+    // const cellCount = tableState.rows[0].cells.length;
+    setTableState((table: TableType) => {
+      table.rows =table?.rows.slice(0,table?.rows.length-1);
+      return { ...table };
+    });
+  };
 
   return (
     <div className="mb-10 bg-blue-50 p-3 rounded-md">
@@ -117,8 +124,15 @@ const TableUI = ({
           >
             Add Row
           </button>
+          <button
+            className=" px-8 py-2 text-white bg-red-500 hover:bg-red-600 font-bold rounded-sm mr-5 mt-2"
+            onClick={deleteRow}
+          >
+            Delete Row
+          </button>
         </div>
       )}
+     
     </div>
 
   );
