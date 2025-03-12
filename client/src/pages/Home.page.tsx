e8ba3d4d3a3504b2b5213809506191bbbf1e6b26
Sumit Das <sumit.kadigital@gmail.com>
2025-03-12 18:26:23 +0530

    Merging with master

--- client/src/pages/Home.page.tsx
+++ client/src/pages/Home.page.tsx
@@ -3,19 +3,15 @@ import AdminLandingPage from "./Admin.landing.page";
 import EntrySectionMain from "./EntrySection";
 import { RootState } from "@/store/store";
 const Homepage = () => {
-  
-  const role =useSelector((state: RootState) => state?.auth?.user?.data.role);
-  
+  const role = useSelector((state: RootState) => state?.auth?.user?.data.role);
+
   return (
-<<<<<<< HEAD
     <div className="h-[100vh]">
-      {role==='SUPERADMIN'||role==='ADMIN'?<AdminLandingPage/>
-      :<EntrySectionMain  />}
-=======
-    <div className="w-[95vw] m-auto ">
-      <FormPage />
-      {/* <SectionB/> */}
->>>>>>> d475055 (added button  and change in data)
+      {role === "SUPERADMIN" || role === "ADMIN" ? (
+        <AdminLandingPage />
+      ) : (
+        <EntrySectionMain />
+      )}
     </div>
   );
 };
