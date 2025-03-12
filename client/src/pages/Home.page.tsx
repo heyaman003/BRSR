97d159235364425ee88237a69ec4b03d63e73bb2
Sumit Das <sumit.kadigital@gmail.com>
2025-03-12 18:20:04 +0530

    added section C tables

--- client/src/pages/Home.page.tsx
+++ client/src/pages/Home.page.tsx
@@ -7,9 +7,15 @@ const Homepage = () => {
   const role =useSelector((state: RootState) => state?.auth?.user?.data.role);
   
   return (
+<<<<<<< HEAD
     <div className="h-[100vh]">
       {role==='SUPERADMIN'||role==='ADMIN'?<AdminLandingPage/>
       :<EntrySectionMain  />}
+=======
+    <div className="w-[95vw] m-auto ">
+      <FormPage />
+      {/* <SectionB/> */}
+>>>>>>> d475055 (added button  and change in data)
     </div>
   );
 };
