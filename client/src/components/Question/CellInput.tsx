3b75882937c4350a2eb0f9999610ca2d0a3194c7
Sumit Das <sumit.kadigital@gmail.com>
2025-03-28 12:25:08 +0530

    Added percentage transformation in Division calculation. Added API for fetching all user mentioins

--- client/src/components/Question/CellInput.tsx
+++ client/src/components/Question/CellInput.tsx
@@ -9,7 +9,7 @@ interface CellInputArgs {
   cellId: string;
   tableState: Table;
   operation: Operation | null | undefined;
-  operands: string[] | null | undefined
+  operands: string[] | null | undefined;
 }
 
 const CellInput: React.FC<CellInputArgs> = ({
@@ -19,28 +19,30 @@ const CellInput: React.FC<CellInputArgs> = ({
   cellId,
   tableState,
   operation,
-  operands
+  operands,
 }) => {
   const [cellData, setCellData] = useState<string>(value);
 
-  const getCellValue = useCallback((rowIndex: number, cellIndex: number)=> {
-    const cellData: string | undefined = tableState.rows
-      .find((row) => row.index === rowIndex)
-      ?.cells.find((cell) => cell.index === cellIndex)?.data;
-    if (!cellData) return 0;
-    return parseFloat(cellData);
-  }, [tableState])
+  const getCellValue = useCallback(
+    (rowIndex: number, cellIndex: number) => {
+      const cellData: string | undefined = tableState.rows
+        .find((row) => row.index === rowIndex)
+        ?.cells.find((cell) => cell.index === cellIndex)?.data;
+      if (!cellData) return 0;
+      return parseFloat(cellData);
+    },
+    [tableState]
+  );
 
-  useEffect(()=>{
-    if(!operation)
-      return;
-    const valueAtIndexes: number[] | undefined = operands?.map(operand=>{
-      const rowIndex = parseFloat(operand.split("$")[0])
-      const colIndex = parseFloat(operand.split("$")[1])
-      return getCellValue(rowIndex, colIndex)
-    })
-    if(valueAtIndexes)
-      setCellData(""+performOperation(operation, valueAtIndexes))
+  useEffect(() => {
+    if (!operation) return;
+    const valueAtIndexes: number[] | undefined = operands?.map((operand) => {
+      const rowIndex = parseFloat(operand.split("$")[0]);
+      const colIndex = parseFloat(operand.split("$")[1]);
+      return getCellValue(rowIndex, colIndex);
+    });
+    if (valueAtIndexes)
+      setCellData("" + performOperation(operation, valueAtIndexes));
   }, [tableState]);
 
   useEffect(() => {
@@ -49,7 +51,7 @@ const CellInput: React.FC<CellInputArgs> = ({
 
   return (
     <input
-      disabled={operation?true:false}
+      disabled={operation ? true : false}
       onChange={(e) => setCellData(e.target.value)}
       value={cellData}
       className=" disabled:bg-gray-50 border disabled:border-gray-300 hover:bg-white focus:bg-white border-green-400 bg-green-50 text-green-900 px-2 py-1 w-full 
@@ -61,15 +63,14 @@ const CellInput: React.FC<CellInputArgs> = ({
 
 export default memo(CellInput);
 
-
-const performOperation = (operation: Operation, values: number[]): number =>{
+const performOperation = (operation: Operation, values: number[]): number => {
   let answer = 0.0;
-  if(operation===Operation.ADD)
-      values.forEach(value=>answer+=value)
-  else{
-      values.forEach((value, ind)=>answer=(ind===0?value:answer/value));
-      if(Number.isNaN(answer) || !Number.isFinite(answer))
-        answer = 0;
+  if (operation === Operation.ADD) values.forEach((value) => (answer += value));
+  else {
+    values.forEach(
+      (value, ind) => (answer = ind === 0 ? value * 100 : answer / value)
+    );
+    if (Number.isNaN(answer) || !Number.isFinite(answer)) answer = 0;
   }
   return answer;
-}
+};
